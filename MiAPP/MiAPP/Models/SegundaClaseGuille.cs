c53d7d89d992a74d68c11c3a2e042b3a6905707d
ghidalga <44208154+ghidalga@users.noreply.github.com>
2019-07-30 11:59:04 +0200

    segundaprueba

---
+++ MiAPP/MiAPP/Models/SegundaClaseGuille.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MiAPP.Models
+{
+  public class SegundaClaseGuille
+  {
+    private bool Error()
+    {
+      return false;
+    }
+  }
+}
